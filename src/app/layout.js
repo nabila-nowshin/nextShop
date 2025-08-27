3396cfe99e7925dd2df366c9af33112c3df12a4c
Nabila Nowshin <nabilanowshin327@gmail.com>
2025-08-28 03:24:53 +0600

    Configure routes

--- src/app/layout.js
+++ src/app/layout.js
@@ -21,11 +21,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className="flex flex-col min-h-screen">
         <Navbar></Navbar>
-        <main>{children}</main>
+
+        <main className="border mt-20 flex-grow ">{children}</main>
 
         <Footer></Footer>
       </body>
